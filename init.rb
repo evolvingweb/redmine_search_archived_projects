febc56e65e14788e0a5fc08b97c80f725b95e9ed
Alex Dergachev <alex@evolvingweb.ca>
2014-11-05 20:51:15 +0000

    Fixed require_dependency paths

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 
-require_dependency "search_archived_application_controller_patch"
 require_dependency "search_archived_projects/hooks"
-require_dependency "search_archived_project_patch"
-require_dependency "search_archived_user_patch"
+require_dependency "search_archived_projects/application_controller_patch"
+require_dependency "search_archived_projects/project_patch"
+require_dependency "search_archived_projects/user_patch"
 
 Redmine::Plugin.register :redmine_search_archived_projects do
   name 'Redmine Search Archived Projects'
