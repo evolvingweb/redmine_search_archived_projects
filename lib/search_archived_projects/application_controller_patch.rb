24d8bb075b934124932d39617b10305ecdba5372
Alex Dergachev <alex@evolvingweb.ca>
2014-11-07 22:27:56 +0000

    Fixed access check error

--- lib/search_archived_projects/application_controller_patch.rb
+++ lib/search_archived_projects/application_controller_patch.rb
@@ -10,14 +10,12 @@ module SearchArchivedProjects
 
     module InstanceMethods
       def check_for_search_archived
-        unless User.current.admin?
-          flash.now[:error] = "Search Archived: can only be activated for admin users."
-          return
-        end
-
-        #TODO: inject JS for new form
-
         if params[:search_archived] == "1"
+          unless User.current.admin?
+            flash.now[:error] = "Search Archived: can only be activated for admin users."
+            return
+          end
+
           flash.now[:alert] = "Search Archived: suspending access checks for admin user."
           $search_archived = params[:search_archived] == "1"
         else
