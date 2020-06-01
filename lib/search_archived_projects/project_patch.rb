5404d36e839f377fdf575287bd1e38a13fbc7c3d
Kevin Porras <kporras07@gmail.com>
2020-06-01 17:38:48 -0600

    refs #20971: Fix to support redmine 4.

--- lib/search_archived_projects/project_patch.rb
+++ lib/search_archived_projects/project_patch.rb
@@ -1,10 +1,19 @@
-module ProjectPatch
-  def allowed_to_condition(user, permission, options={})
-    orig = super(user, permission, options)
-    # remove the filtering-out of archived projects
-    return $search_archived ? orig.sub('projects.status <> 9 AND ', '') : orig
+module SearchArchivedProjects
+  module ProjectPatch
+    def self.included(base) # :nodoc:
+      base.class_eval do
+        def self.allowed_to_condition_with_archived(user, permission, options={})
+          orig = self.allowed_to_condition_without_archived(user, permission, options)
+          # remove the filtering-out of archived projects
+          return $search_archived ? orig.sub('projects.status <> 9 AND ', '') : orig
+        end
+
+        Project.singleton_class.send(:alias_method, :allowed_to_condition_without_archived, :allowed_to_condition)
+        Project.singleton_class.send(:alias_method, :allowed_to_condition, :allowed_to_condition_with_archived)
+
+      end
+    end
   end
 
+  Project.send(:include, ProjectPatch)
 end
-
-Project.prepend ProjectPatch
--- lib/search_archived_projects/user_patch.rb
+++ lib/search_archived_projects/user_patch.rb
@@ -1,10 +1,24 @@
-module UserPatch
-  def allowed_to?(action, context, options={}, &block)
-    if $search_archived && admin?
-      return true
-    end
-    super
-  end
-end
+module SearchArchivedProjects
+  module UserPatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
 
-User.prepend UserPatch
+      base.class_eval do
+        alias_method :allowed_to_without_archived?, :allowed_to?
+        alias_method :allowed_to?, :allowed_to_with_archived?
+      end
+    end
+
+    module InstanceMethods
+      def allowed_to_with_archived?(action, context, options={}, &block)
+        if $search_archived && admin?
+          return true
+        end
+        return allowed_to_without_archived?(action, context, options, &block)
+      end
+
+    end
+  end
+
+  User.send(:include, UserPatch)
+end
