c7c1d9963416767e882ffbf31a7614918678e87f
Burak Ikiler <burak.ikiler@evolvingweb.ca>
2023-04-05 19:49:02 +0300

    Redmine 5 Compatibility: Autoloder fix with to_prepare.

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
 require 'redmine'
 
-require File.dirname(__FILE__) + '/lib/search_archived_projects/hooks'
-require File.dirname(__FILE__) + '/lib/search_archived_projects/application_controller_patch'
-require File.dirname(__FILE__) + '/lib/search_archived_projects/project_patch'
-require File.dirname(__FILE__) + '/lib/search_archived_projects/user_patch'
+Rails.configuration.to_prepare do
+  require_dependency "search_archived_projects/hooks"
+  require_dependency "search_archived_projects/application_controller_patch"
+  require_dependency "search_archived_projects/project_patch"
+  require_dependency "search_archived_projects/user_patch"
+end
 
 Redmine::Plugin.register :redmine_search_archived_projects do
   name 'Redmine Search Archived Projects'
