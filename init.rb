7c49d2b802443234f388654729680e9a46facba0
Burak Ikiler <burak.ikiler@evolvingweb.ca>
2023-04-05 17:16:27 +0300

    Redmine 5 Compatibility: Autoloder fix..

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 
-require_dependency "search_archived_projects/hooks"
-require_dependency "search_archived_projects/application_controller_patch"
-require_dependency "search_archived_projects/project_patch"
-require_dependency "search_archived_projects/user_patch"
+require File.dirname(__FILE__) + '/lib/search_archived_projects/hooks'
+require File.dirname(__FILE__) + '/lib/search_archived_projects/application_controller_patch'
+require File.dirname(__FILE__) + '/lib/search_archived_projects/project_patch'
+require File.dirname(__FILE__) + '/lib/search_archived_projects/user_patch'
 
 Redmine::Plugin.register :redmine_search_archived_projects do
   name 'Redmine Search Archived Projects'
